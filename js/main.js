6c566198705eca2f61f6439659ee5e0387ef1fbb
marysiaK <marysia.kolaczynska@gmail.com>
2017-03-06 19:55:24 +0100

    basic main nav, add .sass-cache to gitignore

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $( document ).ready(function() {
+	
+	// main navigation visibility on scroll
 	$(window).scroll(function() {
 	    if ($(document).scrollTop() > 300) {
 	        console.log("make main-nav visible here");
@@ -8,4 +10,36 @@ $( document ).ready(function() {
 	    	$('.main-nav').removeClass('main-nav--is-visible');
 	    }
 	});
-});
+
+	// skills-circle
+	$('.skills-circle').percentcircle({
+
+		animate : true,
+		diameter : 100,
+		guage: 20,
+		coverBg: '#fff',
+		bgColor: '#939393',
+		fillColor: '#F7CE3E',
+		percentSize: '15px',
+		percentWeight: 'normal'
+
+	});
+
+	// mobile navigation
+// 		$( ".cross" ).hide();
+// 		$( ".main-nav__links" ).hide();
+// 		$('.hamburger-menu').click (function() { 
+// 		$('.main-nav__links').slideToggle( "slow", function() {
+// 		$( ".hamburger-menu" ).hide();
+// 		$( ".cross" ).show();
+// 		});
+// 		});
+
+// 		$( ".cross" ).click(function() {
+// 		$( ".main-nav__links" ).slideToggle( "slow", function() {
+// 		$( ".cross" ).hide();
+// 		$( ".hamburger-menu" ).show();
+// 		});
+// 		});	
+		
+ });
