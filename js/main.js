64349225dadb5e6495527c52765100e38a96deca
marysiaK <marysia.kolaczynska@gmail.com>
2017-03-11 09:52:53 +0100

    form styles

--- js/main.js
+++ js/main.js
@@ -34,4 +34,25 @@ $( document ).ready(function() {
 	$('.main-nav__link').click(function() { 
 		$('.main-nav').removeClass('mobile-nav--open');
 	});
+
+	// scroll reveal
+	window.sr = ScrollReveal();
+	sr.reveal('.page-section', { duration: 1000, distance: '50px' });
+
+	// smooth scroll
+
+	$(function() {
+	  $('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
  });
