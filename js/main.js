84a94c8ba0b9fb77815ecd70333eed68dbeae366
marysiaK <marysia.kolaczynska@gmail.com>
2017-04-24 20:45:49 +0200

    all portfolio sections

--- js/main.js
+++ js/main.js
@@ -1,58 +1,44 @@
 $( document ).ready(function() {
-	
-	// main navigation visibility on scroll
-	$(window).scroll(function() {
-	    if ($(document).scrollTop() > 300) {
-	        console.log("make main-nav visible here");
-	        $('.main-nav').addClass('main-nav--is-visible');
+    
+    // main navigation visibility on scroll
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 300) {
+            console.log("make main-nav visible here");
+            $('.main-nav').addClass('main-nav--is-scrolled-down');
 
-	    } else {
-	    	$('.main-nav').removeClass('main-nav--is-visible');
-	    }
-	});
+        } else {
+            $('.main-nav').removeClass('main-nav--is-scrolled-down');
+        }
+    });
 
-	// skills-circle
-	$('.skills-circle').percentcircle({
+    // mobile navigation
 
-		animate : true,
-		diameter : 100,
-		guage: 20,
-		coverBg: '#fff',
-		bgColor: '#939393',
-		fillColor: '#F7CE3E',
-		percentSize: '15px',
-		percentWeight: 'normal'
+    $('.hamburger-menu').click(function() { 
+        $('.main-nav').toggleClass('mobile-nav--open');
+    });
 
-	});
+    $('.main-nav__link').click(function() { 
+        $('.main-nav').removeClass('mobile-nav--open');
+    });
 
-	// mobile navigation
+    // scroll reveal
+    window.sr = ScrollReveal();
+    sr.reveal('.page-section', { duration: 1000, distance: '50px' });
 
-	$('.hamburger-menu').click(function() { 
-		$('.main-nav').toggleClass('mobile-nav--open');
-	});
+    // smooth scroll
 
-	$('.main-nav__link').click(function() { 
-		$('.main-nav').removeClass('mobile-nav--open');
-	});
-
-	// scroll reveal
-	window.sr = ScrollReveal();
-	sr.reveal('.page-section', { duration: 1000, distance: '50px' });
-
-	// smooth scroll
-
-	$(function() {
-	  $('a[href*="#"]:not([href="#"])').click(function() {
-	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-	      var target = $(this.hash);
-	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	      if (target.length) {
-	        $('html, body').animate({
-	          scrollTop: target.offset().top
-	        }, 1000);
-	        return false;
-	      }
-	    }
-	  });
-	});
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
  });
