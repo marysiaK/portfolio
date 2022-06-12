0bb03e343aafdb28336a636e7d3eef76a9c7096a
marysiaK <marysia.kolaczynska@gmail.com>
2022-06-12 16:36:19 +0200

    refresh portfolio

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+    
+    // main navigation visibility on scroll
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 100) {
+            console.log("make main-nav visible here");
+            $('.main-nav').addClass('main-nav--is-scrolled-down');
+
+        } else {
+            $('.main-nav').removeClass('main-nav--is-scrolled-down');
+        }
+    });
+
+    // mobile navigation
+    $('.hamburger-menu').click(function()  { 
+        $('.main-nav').toggleClass('mobile-nav--open');
+    });
+
+    $('.main-nav__link').click(function() { 
+        $('.main-nav').removeClass('mobile-nav--open');
+    });
+
+    // scroll reveal
+    window.sr = ScrollReveal();
+    sr.reveal('.page-section', { duration: 1000, distance: '0', viewFactor: 0.2, mobile: false });
+
+    // smooth scroll
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top -110,
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+ });
